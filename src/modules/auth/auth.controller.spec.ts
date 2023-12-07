ca11df0a5ceb0d45122b6fa5fa073d2da8a7958a
DanhVanPham <Danhskipper18@gmail.com>
2023-12-07 23:19:11 +0700

    test(dev): learn unit test

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -1,17 +1,23 @@
-import { Test, TestingModule } from '@nestjs/testing';
+// import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '@modules/users/users.service';
+import { UsersRepository } from '@repositories/users.repository';
+import { User } from '@modules/users/entities/user.entity';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController],
-      providers: [AuthService],
-    }).compile();
-
-    controller = module.get<AuthController>(AuthController);
+    controller = new AuthController(
+      new AuthService(
+        new ConfigService(),
+        new JwtService(),
+        new UsersService(new UsersRepository(User as any)),
+      ),
+    );
   });
 
   it('should be defined', () => {
