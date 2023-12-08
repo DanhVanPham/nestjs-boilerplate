32a797c6bfa5c3991b5cea3dea745188b88744b9
Danh Pham <phamdanh@v-takeuchi.vn>
2023-12-08 08:27:47 +0700

    Test

--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -1,18 +1,26 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '@modules/users/users.service';
+import { UsersRepository } from '@repositories/users.repository';
+import { User } from '@modules/users/entities/user.entity';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  const auth_service: AuthService = new AuthService(
+    new ConfigService(),
+    new JwtService(),
+    new UsersService(new UsersRepository(User as any)),
+  );
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+  // beforeEach(async () => {
+  //   const module: TestingModule = await Test.createTestingModule({
+  //     providers: [AuthService],
+  //   }).compile();
 
-    service = module.get<AuthService>(AuthService);
-  });
+  //   service = module.get<AuthService>(AuthService);
+  // });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(auth_service).toBeDefined();
   });
 });
